ca17541458c6faa0c584813d85d2681c21019142
CelestialAbyss <30322176+CelestialAbyss@users.noreply.github.com>
2023-08-02 03:26:03 -0400

    Merged Create.js and implementing custom toasts
    - Laying down the framework for toast notifications
    - Toasts in progress

--- kubejs/server_scripts/genesis_server.js
+++ kubejs/server_scripts/genesis_server.js
@@ -1,6 +1,6 @@
 // priority: 0
 
-console.info('Hello, World! (You will see this line every time server resources reload)')
+console.info('Implementing Finality recipes and compats...')
 
 let STONEPLATES = ['stone', 'polished_blackstone']
 let WOODPLATES = ['oak', 'spruce', 'birch', 'jungle', 'acacia', 'dark_oak', 'mangrove', 'crimson', 'warped']
@@ -173,6 +173,116 @@ ServerEvents.recipes(event => {
         R: 'minecraft:redstone'
     }).id('minecraft:heavy_weighted_pressure_plate')
     event.shapeless('create:mechanical_piston', ['supplementaries:soap', 'create:sticky_mechanical_piston']).id('finality:mechanical_piston_soap_washing')
+    // compacting
+    event.recipes.createCompacting([
+        'minecraft:sponge',
+        Fluid.of('minecraft:water', 1000)
+    ], [
+        'minecraft:wet_sponge'
+    ]).id('finality:sponge_squeezing')
+    event.recipes.createCompacting([
+        'minecraft:diamond'
+    ], [
+        Item.of('minecraft:coal_block', 1),
+        Fluid.of('minecraft:lava', 250)
+    ]).superheated().id('finality:renew_diamond')
+    event.recipes.createCompacting([
+        'minecraft:diamond_block'
+    ], [
+        Item.of('minecraft:coal_block', 9),
+        Fluid.of('minecraft:lava', 250)
+    ]).superheated().id('finality:renew_diamond_bulk')
+    event.recipes.createCompacting([
+        'minecraft:coal'
+    ], [
+        Item.of('minecraft:dried_kelp_block', 1)
+    ]).heated().id('finality:renew_coal')
+    event.recipes.createCompacting([
+        'minecraft:coal_block'
+    ], [
+        Item.of('minecraft:dried_kelp_block', 9)
+    ]).heated().id('finality:renew_coal_bulk')
+    event.recipes.createCompacting([
+        'minecraft:basalt'
+    ], [
+        'minecraft:blue_ice',
+        Fluid.of('minecraft:lava', 500)
+    ]).id('finality:compacting_basalt')
+    event.recipes.createCompacting([
+        'minecraft:tuff'
+    ], [
+        Item.of('minecraft:gravel', 18),
+        Fluid.of('minecraft:lava', 250)
+    ]).superheated().id('finality:renew_tuff')
+    event.recipes.createCompacting([
+        'minecraft:tuff'
+    ], [
+        Item.of('minecraft:deepslate', 9),
+        Fluid.of('minecraft:lava', 250)
+    ]).heated().id('finality:renew_deepslate_tuff')
+    // crushing
+    event.remove({ id: 'create:crushing/gravel' })
+    event.remove({ id: 'create:crushing/netherrack' })
+    event.recipes.createCrushing([
+        'minecraft:dirt',
+        Item.of('minecraft:flint').withChance(0.10)
+    ], 'minecraft:gravel').processingTime(250).id('finality:gravel_crushing')
+    event.recipes.createCrushing([
+        'minecraft:sand',
+        Item.of('minecraft:clay_ball').withChance(0.05)
+    ], 'minecraft:dirt').processingTime(250).id('finality:dirt_crushing')
+    event.recipes.createCrushing([
+        Item.of('minecraft:pointed_dripstone', 4),
+        Item.of('minecraft:clay_ball').withChance(0.50),
+        Item.of('create:copper_nugget').withChance(0.25),
+        Item.of('create:experience_nugget').withChance(0.75)
+    ], 'minecraft:dripstone_block').processingTime(250).id('finality:dripstone_crushing')
+    event.recipes.createCrushing([
+        Item.of('create:copper_nugget').withChance(0.05),
+        Item.of('minecraft:gunpowder').withChance(0.10)
+    ], 'minecraft:basalt').id('finality:basalt_crushing')
+    event.recipes.createCrushing([
+        'create:cinder_flour',
+        Item.of('create:cinder_flour').withChance(0.50),
+        Item.of('minecraft:glowstone_dust').withChance(0.25),
+        Item.of('minecraft:netherite_scrap').withChance(0.002)
+    ], 'minecraft:netherrack').processingTime(250).id('finality:netherrack_crushing')
+    // filling
+    event.recipes.createFilling('minecraft:netherrack', [
+        'minecraft:cobblestone',
+        Fluid.of('create:potion', 250, '{Potion: "minecraft:strong_healing"}')
+    ]).id('finality:living_flesh_stone')
+    // haunting
+    event.recipes.createHaunting(['minecraft:deepslate'], 'minecraft:andesite').id('finality:andesite_haunting')
+    event.recipes.createHaunting(['minecraft:crying_obsidian'], 'minecraft:obsidian').id('finality:obsidian_haunting')
+    event.recipes.createHaunting(['minecraft:name_tag'], 'minecraft:writable_book').id('finality:soul_nametag')
+    // splashing
+    event.recipes.createSplashing(['minecraft:piston'], 'minecraft:sticky_piston').id('finality:sticky_piston_splashing')
+    event.recipes.createSplashing(['create:mechanical_piston'], 'create:sticky_mechanical_piston').id('finality:sticky_mechanical_piston_splashing')
+    // blasting
+    event.blasting('create:zinc_block', 'create:raw_zinc_block').id('finality:zinc_block_blasting_compat')
+    // mixing 
+    event.recipes.createMixing([
+        'minecraft:coarse_dirt'
+    ], [
+        'minecraft:dirt', 'minecraft:gravel'
+    ]).id('finality:coarse_dirt')
+    // Be3Al2(SiO3)6
+    event.recipes.createMixing([
+        'minecraft:emerald'
+    ], [
+        'minecraft:quartz',
+        'minecraft:glass',
+        '3x minecraft:iron_nugget'
+    ]).superheated().id('finality:renew_emerald')
+    // Thank you to FunnyMan4579 on the official Create Discord for giving me this idea :3
+    event.recipes.createMixing([
+        'minecraft:nether_gold_ore'
+    ], [
+        'create:cinder_flour',
+        '18x minecraft:gold_nugget',
+        Fluid.of('minecraft:lava', 250)
+    ]).id('finality:nether_gold_ore_deco')
 })
 
 ServerEvents.tags('item', event => {
@@ -183,29 +293,50 @@ ServerEvents.tags('item', event => {
     // event.get('forge:cobblestone').remove('minecraft:mossy_cobblestone')
 })
 
+PlayerEvents.loggedIn(event => {
+    //Give the player the quest book on first join
+    if (!event.player.persistentData.contains('firstjoin')) {
+        event.player.persistentData.putBoolean('firstjoin', true)
+        event.player.give('ftbquests:book')
+    }
+})
+
+ServerEvents.loaded(event => {
+    if (!event.server.persistentData.contains('firstload')) {
+        event.server.persistentData.putBoolean('firstload', true)
+        console.warn('First server load! Lag may be present for a few minutes. Also, please check your Quest Book to get your bearings.')
+    }
+
+})
+
 const set = {
     "name": "kubejs:final",
     "effects": [
         {
             "effect": "saturation",
-            "duration": 400,
-            "amplifier": 0
+            "duration": 999,
+            "amplifier": 255
         },
         {
             "effect": "regeneration",
-            "duration": 400,
+            "duration": 999,
+            "amplifier": 255
+        },
+        {
+            "effect": "instant_health",
+            "duration": 999,
             "amplifier": 255
         },
         {
             "effect": "resistance",
-            "duration": 400,
+            "duration": 999,
             "amplifier": 255
         },
     ]
 }
 
 const sets = [set];
-
+// Need to figure out how to set this up for EntityEvents.hurt()
 PlayerEvents.tick(e => {
     const { headArmorItem, chestArmorItem, legsArmorItem, feetArmorItem } = e.player;
     if (e.player.level.time % 100 == 0) {
@@ -221,3 +352,48 @@ PlayerEvents.tick(e => {
         }
     };
 });
+
+// EntityEvents.hurt(event => {
+//     if (event.entity.player.armorSlots() === 'kubejs:final_helmet' && 'kubejs:final_chestplate' && 'kubejs:final_leggings' && 'kubejs:final_boots') {
+//         console.log('69')
+//         event.cancel()
+//     }
+// })
+
+// let CLOCK = 0
+// let sentience = [repairHint, worldMaintenance, ]
+// ServerEvents.tick(event => {
+//     
+// })
+
+EntityEvents.death('minecraft:wither', event => {
+    event.player.notify(Notification.make(toast => {
+        toast.text = [
+            Text.of("The Wither has been killed!").bold(),
+            Text.of('subtitle')
+        ]
+        toast.icon = 'minecraft:wither_skeleton_skull'
+        toast.outlineColor = '#006055'
+        toast.backgroundColor = '#1b3a1b'
+        toast.borderColor = '#267523'
+    }))
+})
+
+BlockEvents.rightClicked('minecraft:bedrock', event => {
+    event.entity.notify(Notification.make(n => {
+        n.text = [
+            'Why?',
+            'subtitle'
+        ]
+        n.icon = 'minecraft:bedrock'
+        n.outlineColor = '#006055'
+        n.backgroundColor = '#1b3a1b'
+        n.borderColor = '#267523'
+    }))
+})
+
+// EntityEvents.death('minecraft:wither', event => event.player.notify({
+//     itemIcon: 'minecraft:wither_skeleton_skull',
+//     backgroundColor: 'darkPurple',
+//     borderColor: '0x'
+// }))
