cfb07f2be0e3fad490fac3da18f9d4b628f911a3
CelestialAbyss <30322176+CelestialAbyss@users.noreply.github.com>
2023-08-13 20:20:00 -0400

    Cutting board script changed

--- kubejs/server_scripts/recipes/farmersdelight.js
+++ kubejs/server_scripts/recipes/farmersdelight.js
@@ -1,14 +1,6 @@
 // requires: farmersdelight
 
-let oak = 'minecraft:oak_slab'
-let spruce = 'minecraft:spruce_slab'
-let birch = 'minecraft:birch_slab'
-let jungle = 'minecraft:jungle_slab'
-let acacia = 'minecraft:acacia_slab'
-let darkoak = 'minecraft:dark_oak_slab'
-let crimson = 'minecraft:crimson_slab'
-let warped = 'minecraft:warped_slab'
-let fdBoard = 'farmersdelight:cutting_board'
+let WOOD = ['oak', 'spruce', 'birch', 'jungle', 'acacia', 'dark_oak', 'crimson', 'warped']
 
 ServerEvents.recipes(event => {
     event.remove({id: 'farmersdelight:skillet'})
@@ -35,11 +27,7 @@ ServerEvents.recipes(event => {
     // hashtag minecraft:wooden_slabs does not work apparently so this is how it is going to be
     // KubeJS Create has now been updated, enjoy
     // Disabled until kubejs create is updated
-    event.recipes.createCutting(fdBoard, [Item.of(oak)]).processingTime(100).id('finality:farmersdelight_cutting_board_oak')
-    event.recipes.createCutting(fdBoard, [Item.of(spruce)]).processingTime(100).id('finality:farmersdelight_cutting_board_spruce')
-    event.recipes.createCutting(fdBoard, [Item.of(jungle)]).processingTime(100).id('finality:farmersdelight_cutting_board_jungle')
-    event.recipes.createCutting(fdBoard, [Item.of(acacia)]).processingTime(100).id('finality:farmersdelight_cutting_board_acacia')
-    event.recipes.createCutting(fdBoard, [Item.of(darkoak)]).processingTime(100).id('finality:farmersdelight_cutting_board_dark_oak')
-    event.recipes.createCutting(fdBoard, [Item.of(crimson)]).processingTime(100).id('finality:farmersdelight_cutting_board_crimson')
-    event.recipes.createCutting(fdBoard, [Item.of(warped)]).processingTime(100).id('finality:farmersdelight_cutting_board_warped')
+    WOOD.forEach(type => {
+        event.recipes.createCutting('farmersdelight:cutting_board', `minecraft:${type}_slab`).processingTime(100).id(`finality:farmersdelight/cutting_board/${type}`)
+    })
 })
