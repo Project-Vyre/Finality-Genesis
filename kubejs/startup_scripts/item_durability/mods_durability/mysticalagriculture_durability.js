63b461e8c220bc38d588874b4acc04116c61e163
CelestialAbyss <30322176+CelestialAbyss@users.noreply.github.com>
2023-08-13 21:03:36 -0400

    Mystical Agriculture durability script fixed.

--- kubejs/startup_scripts/item_durability/mods_durability/mysticalagriculture_durability.js
+++ kubejs/startup_scripts/item_durability/mods_durability/mysticalagriculture_durability.js
@@ -1,29 +1,48 @@
-let ARMOR = ['helmet','chestplate','leggings','boots']
-let TOOLS = ['sword', 'shovel', 'pickaxe', 'axe', 'hoe']
-let inferium_durability = 2048
-let prudentium_durability = 4096
-let tertium_durability = 4096
-let imperium_durability = 8192
+// requires: mysticalagriculture
+
+let INFERIUM_TOOLS = 2048
+let PRUDENTIUM_TOOLS = 4096
+let TERTIUM_TOOLS = 4096
+let IMPERIUM_TOOLS = 8192
+
+let INFERIUM_ARMOR = 1024
+let PRUDENTIUM_ARMOR = 2048
+let TERTIUM_ARMOR = 2048
+let IMPERIUM_ARMOR = 4096
 
 ItemEvents.modification(event => {
     TOOLS.forEach(instool => {
         event.modify(`mysticalagriculture:inferium_${instool}`, item => {
-            item.maxDamage = inferium_durability
+            item.maxDamage = INFERIUM_TOOLS
         })
-    })
-    TOOLS.forEach(instool => {
         event.modify(`mysticalagriculture:prudentium_${instool}`, item => {
-            item.maxDamage = prudentium_durability
+            item.maxDamage = PRUDENTIUM_TOOLS
         })
-    })
-    TOOLS.forEach(instool => {
         event.modify(`mysticalagriculture:tertium_${instool}`, item => {
-            item.maxDamage = tertium_durability
+            item.maxDamage = TERTIUM_TOOLS
+        })
+        event.modify(`mysticalagriculture:imperium_${instool}`, item => {
+            item.maxDamage = IMPERIUM_TOOLS
         })
     })
-    TOOLS.forEach(instool => {
-        event.modify(`mysticalagriculture:imperium_${instool}`, item => {
-            item.maxDamage = imperium_durability
+    ARMOR.forEach(insarmor => {
+        event.modify(`mysticalagriculture:inferium_${insarmor}`, item => {
+            item.maxDamage = INFERIUM_ARMOR
+        })
+        event.modify(`mysticalagriculture:prudentium_${insarmor}`, item => {
+            item.maxDamage = PRUDENTIUM_ARMOR
+        })
+        event.modify(`mysticalagriculture:tertium_${insarmor}`, item => {
+            item.maxDamage = TERTIUM_ARMOR
+        })
+        event.modify(`mysticalagriculture:imperium_${insarmor}`, item => {
+            item.maxDamage = IMPERIUM_ARMOR
+        })
+        event.modify(`mysticalagriculture:supremium_${insarmor}`, item => {
+            item.maxDamage = -1
+        })
+        event.modify(`mysticalagriculture:awakened_supremium_${insarmor}`, item => {
+            item.maxDamage = -1
         })
     })
 })
