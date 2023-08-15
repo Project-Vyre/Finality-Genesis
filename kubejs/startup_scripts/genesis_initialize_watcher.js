90749b70452e32e8a31238d554260da3d12e6d92
CelestialAbyss <30322176+CelestialAbyss@users.noreply.github.com>
2023-08-15 03:56:58 -0400

    Genesis Version Watcher implementation.

---
+++ kubejs/startup_scripts/genesis_initialize_watcher.js
@@ -0,0 +1,5 @@
+StartupEvents.init(event => {
+    let current = JsonIO.read('kubejs/update_notifier.json') ?? {}
+    current["is_notified_at_this_launch"] = false
+    JsonIO.write('kubejs/update_notifier.json', current)
+})
