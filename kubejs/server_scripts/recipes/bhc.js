166e06be0d637d9e258ef402d55f0c535cf483b8
CelestialAbyss <30322176+CelestialAbyss@users.noreply.github.com>
2023-08-15 05:08:43 -0400

    BHC recipes updated.
    - Canister now requires 4 iron sheets and 1 soul sand to create.
    - Relic apples now require golden apples.
    - Relic apples can also be made shapelessly.
    - Wither bones can now be acquired by haunting bones.

---
+++ kubejs/server_scripts/recipes/bhc.js
@@ -0,0 +1,33 @@
+// requires: bhc
+// requires: create
+// requires: supplementaries
+
+ServerEvents.recipes(event => {
+    event.shaped('bhc:canister', [
+        ' I ',
+        'ICI',
+        ' I '
+    ], {
+        I: 'create:iron_sheet',
+        C: 'minecraft:soul_sand'
+    }).id('bhc:canister')
+    event.shaped('bhc:relic_apple', [
+        ' D ',
+        'EAE',
+        ' D '
+    ], {
+        D: 'minecraft:diamond',
+        E: 'minecraft:emerald',
+        A: 'minecraft:golden_apple'
+    }).id('bhc:relic_apple')
+    event.shapeless('bhc:relic_apple', [
+        'minecraft:diamond',
+        'minecraft:emerald',
+        'minecraft:golden_apple'
+    ]).id('finality:shapeless_mixing_compat')
+    event.recipes.create.haunting([
+        Item.of('bhc:wither_bone').withChance(0.25),
+        Item.of('minecraft:coal').withChance(0.25),
+        Item.of('supplementaries:ash').withChance(0.25)
+    ], 'minecraft:bone').id('finality:bhc/haunting/wither_bone')
+})
