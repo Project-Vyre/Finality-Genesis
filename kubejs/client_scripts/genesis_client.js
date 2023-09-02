8f6f367366c1ce939840398047e31ea1d987085a
CelestialAbyss <30322176+CelestialAbyss@users.noreply.github.com>
2023-09-02 06:11:45 -0400

    014 Patch D added tooltip for Chalk Box

--- kubejs/client_scripts/genesis_client.js
+++ kubejs/client_scripts/genesis_client.js
@@ -385,6 +385,35 @@ ItemEvents.tooltip(event => {
                 ])
             }
         })
+        event.addAdvanced('chalk:chalk_box', (item, advanced, text) => {
+            if (!event.shift) {
+                text.add(1, [
+                    Text.of('Hold ').gray(),
+                    Text.of('[Shift] ').white(),
+                    Text.of('to see more info.').gray()
+                ])
+            } else {
+                text.add(1, [
+                    Text.of('Click on the ').gold(),
+                    Text.of('full side ').yellow(),
+                    Text.of('of a block to draw a mark.').gold()
+                ])
+                text.add(2, [
+                    Text.of('The ').gold(),
+                    Text.of('direction ').red(),
+                    Text.of('does matter and will point the arrow in that direction.').gold()
+                ])
+                text.add(3, [
+                    Text.of('The ').gold(),
+                    Text.of('special slot ').green(),
+                    Text.of('is used for ').gold(),
+                    Text.of('glow ink sacs ').aqua(),
+                    Text.of('to make ').gold(),
+                    Text.of('glowing ').aqua(),
+                    Text.of('chalk marks.').gold()
+                ])
+            }
+        })
     }
     if (Platform.isLoaded('salt')) {
         event.addAdvanced('salt:salt', (item, advanced, text) => {
