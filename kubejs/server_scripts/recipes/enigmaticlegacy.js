372cae391cb17670e1d2aa612fde958485bdcef9
CelestialAbyss <30322176+CelestialAbyss@users.noreply.github.com>
2023-09-02 15:20:37 -0400

    Implemented new recipes for Etherium...

--- kubejs/server_scripts/recipes/enigmaticlegacy.js
+++ kubejs/server_scripts/recipes/enigmaticlegacy.js
@@ -2,6 +2,18 @@
 // requires: extendedcrafting
 
 ServerEvents.recipes(event => {
+    event.recipes.create.haunting([
+        'enigmaticlegacy:etherium_scraps',
+        Item.of('create:experience_nugget').withChance(0.25)
+    ], 'minecraft:netherite_scrap').id('finality:etherium_scraps')
+    event.recipes.create.crushing([
+        Item.of('enigmaticlegacy:etherium_ore').withChance(0.50),
+        Item.of('create:experience_nugget').withChance(0.25)
+    ], 'minecraft:end_stone').id('finality:etherium_ore_from_end_stone')
+    event.recipes.create.mixing('enigmaticlegacy:etherium_ingot', [
+        '4x enigmaticlegacy:etherium_scraps',
+        '2x create:powdered_obsidian'
+    ]).superheated().id('finality:etherium_ingot_creation')
     event.custom({
         type: 'extendedcrafting:combination',
         powerCost: 5000000,
