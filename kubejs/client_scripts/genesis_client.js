5bd57b31db96d86dfca4efbe4c2c141b0f734239
CelestialAbyss <30322176+CelestialAbyss@users.noreply.github.com>
2023-10-10 19:38:05 -0400

    [0.2-rc3a] Uhhhh...

--- kubejs/client_scripts/genesis_client.js
+++ kubejs/client_scripts/genesis_client.js
@@ -6,6 +6,7 @@
  * Authors
  * 
  * @squoshi Original screenshake script author
+ * @ChiefArug Client mod blacklist
  * @CelestialAbyss
  */
 
@@ -19,3 +20,30 @@ NetworkEvents.dataReceived('screenshake', event => {
     const { i1, i2, i3, duration } = event.data
     $ScreenshakeHandler.addScreenshake($ScreenshakeInstance(duration).setIntensity(i1, i2, i3).setEasing($Easing.SINE_IN, $Easing.QUAD_IN))
 })
+
+/**
+ * Hello script file editor. Please don't disable this check unless you really know what you are doing,
+ * and are aware that playing with these mods may cause everything from mild lore spoilers to full world corruption.
+ * 
+ * - ChiefArug
+ * 
+ * *cough* OptiFine *cough*
+ * 
+ * - CelestialAbyss
+ */
+
+const BAD_MODS = ['optifine']
+
+let badModsLoaded = BAD_MODS.filter(mod => Platform.getMods().keySet().contains(mod))
+
+if (badModsLoaded.length > 0) {
+    ClientEvents.loggedIn(event => {
+        Client.gui.setTimes(20, 200000, 20)
+        Client.gui.setTitle(Text.red("INCOMPATIBLE MODS FOUND!"))
+        Client.gui.setSubtitle(Text.darkRed("Please remove these mods: " + badModsLoaded))
+        event.player.paint({
+            blackout:
+                { type: 'rectangle', x: 0, y: 0, z: -100, w: 1000, h: 1000, draw: 'always', alignX: 'center', alignY: 'center' }
+        })
+    })
+}
