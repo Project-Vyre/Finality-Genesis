0201fceb4c36ee62f7c8323f29c03f2862c1ca37
CelestialAbyss <30322176+CelestialAbyss@users.noreply.github.com>
2023-11-21 03:23:36 -0500

    Moved MoreJS trades to trades handler

--- kubejs/server_scripts/genesis_trades.js
+++ kubejs/server_scripts/genesis_trades.js
@@ -36,6 +36,32 @@ MoreJSEvents.wandererTrades(event => {
             'minecraft:emerald'
         ], 'minecraft:warden_spawn_egg')
     }
+    if (Platform.isLoaded('cataclysm')) {
+        event.addTrade(2, [
+            'minecraft:emerald_block',
+            'cataclysm:monstrous_eye'
+        ], 'cataclysm:netherite_monstrosity_spawn_egg')
+        event.addTrade(2, [
+            'minecraft:emerald_block',
+            'cataclysm:flame_eye'
+        ], 'cataclysm:ignis_spawn_egg')
+        event.addTrade(2, [
+            'minecraft:emerald_block',
+            'cataclysm:mech_eye'
+        ], 'cataclysm:the_harbinger_spawn_egg')
+        event.addTrade(2, [
+            'minecraft:emerald_block',
+            'cataclysm:void_eye'
+        ], 'cataclysm:ender_guardian_spawn_egg')
+        event.addTrade(2, [
+            'minecraft:emerald_block',
+            'cataclysm:abyss_eye'
+        ], 'cataclysm:the_leviathan_spawn_egg')
+        event.addTrade(2, [
+            'minecraft:emerald_block',
+            'minecraft:amethyst_block'
+        ], 'cataclysm:amethyst_crab_spawn_egg')
+    }
     if (Platform.isLoaded('caupona')) {
         event.addTrade(1, [
             '8x minecraft:clay_ball',
