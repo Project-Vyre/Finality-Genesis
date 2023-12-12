4d9cff3e97ecfb03e6e3e90382076990ab54cf64
CelestialAbyss <30322176+CelestialAbyss@users.noreply.github.com>
2023-12-12 17:00:35 -0500

    More tooltips removed to be replaced with Create tooltips

--- kubejs/client_scripts/tooltip_removal.js
+++ kubejs/client_scripts/tooltip_removal.js
@@ -79,4 +79,32 @@ ItemEvents.tooltip(event => {
             if (item.hasNBT()) { text.remove(1) }
         })
     }
+    if (Platform.isLoaded('bosses_of_mass_destruction')) {
+        let bomd_items = [
+            'soul_star',
+            'ancient_anima',
+            'blazing_eye',
+            'obsidian_heart',
+            'earthdive_spear',
+            'void_thorn',
+            'crystal_fruit',
+            'brimstone_nectar',
+            'mob_ward',
+            'levitation_block',
+            'void_lily'
+        ]
+        bomd_items.forEach(itemTooltip => {
+            event.addAdvanced(`bosses_of_mass_destruction:${itemTooltip}`, (item, advanced, text) => {
+                text.remove(1)
+            })
+        })
+        event.addAdvanced('bosses_of_mass_destruction:charged_ender_pearl', (item, advanced, text) => {
+            text.remove(1)
+            text.remove(1)
+        })
+        event.addAdvanced('bosses_of_mass_destruction:monolith_block', (item, advanced, text) => {
+            text.remove(1)
+            text.remove(1)
+        })
+    }
 })
