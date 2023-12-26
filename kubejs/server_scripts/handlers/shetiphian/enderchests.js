c3c62107f29d1ebe40dd6ef05d31b28772c001e9
CelestialAbyss <30322176+CelestialAbyss@users.noreply.github.com>
2023-12-26 07:01:29 -0500

    Finished EnderChests and EnderTanks related scripts...
    - Also added tooltips.

--- kubejs/server_scripts/handlers/shetiphian/enderchests.js
+++ kubejs/server_scripts/handlers/shetiphian/enderchests.js
@@ -1,2 +1,55 @@
 // requires: enderchests
 // requires: create
+
+/**
+ * @file Server handler for ShetiPhian's EnderChests mod.
+ * @author CelestialAbyss <https://github.com/CelestialAbyss> Modpack lead
+ */
+
+ServerEvents.tags('block', event => {
+    event.add('create:wrench_pickup', 'enderchests:ender_chest')
+})
+
+ServerEvents.recipes(event => {
+    event.remove([
+        'enderchests:ender_bag',
+        'enderchests:ender_chest',
+        'enderchests:ender_pouch'
+    ])
+    if (Platform.isLoaded('extendedcrafting')) {
+        // ender bag
+        event.shaped('enderchests:ender_bag', [
+            'AWA',
+            'OPO',
+            'AEA'
+        ], {
+            A: 'extendedcrafting:advanced_component',
+            W: '#minecraft:wool',
+            O: 'create:powdered_obsidian',
+            P: 'enderchests:ender_pouch',
+            E: 'extendedcrafting:enhanced_ender_catalyst'
+        }).id('finality:shetiphian/ender_bag')
+        // enderchests ender chest
+        event.shaped('enderchests:ender_chest', [
+            'AWA',
+            'OCO',
+            'AEA'
+        ], {
+            A: 'extendedcrafting:advanced_component',
+            W: '#minecraft:wool',
+            O: 'kubejs:high_entropy_alloy_sheet',
+            C: 'minecraft:ender_chest',
+            E: 'extendedcrafting:enhanced_ender_catalyst'
+        }).id('finality:shetiphian/ender_chest')
+        // ender pouch
+        event.shaped('enderchests:ender_pouch', [
+            'ALA',
+            'LEL',
+            'LLL'
+        ], {
+            A: 'minecraft:gold_nugget',
+            L: 'minecraft:leather',
+            E: 'minecraft:ender_eye'
+        }).id('finality:shetiphian/ender_pouch')
+    }
+})
--- kubejs/server_scripts/handlers/shetiphian/endertanks.js
+++ kubejs/server_scripts/handlers/shetiphian/endertanks.js
@@ -1,2 +1,44 @@
 // requires: endertanks
 // requires: create
+
+/**
+ * @file Server handler for ShetiPhian's EnderTanks mod.
+ * @author CelestialAbyss <https://github.com/CelestialAbyss> Modpack lead
+ */
+
+ServerEvents.tags('block', event => {
+    event.add('create:wrench_pickup', 'endertanks:tank')
+})
+
+ServerEvents.recipes(event => {
+    event.remove([
+        'endertanks:bucket',
+        'endertanks:tank'
+    ])
+    if (Platform.isLoaded('extendedcrafting')) {
+        // ender bucket
+        event.shaped('endertanks:ender_bucket', [
+            'AWA',
+            'OBO',
+            'AEA'
+        ], {
+            A: 'extendedcrafting:advanced_component',
+            W: '#minecraft:wool',
+            O: 'kubejs:high_entropy_alloy_sheet',
+            B: 'minecraft:bucket',
+            E: 'extendedcrafting:enhanced_ender_catalyst'
+        }).id('finality:shetiphian/ender_bucket')
+        // ender tank
+        event.shaped('endertanks:ender_tank', [
+            'AWA',
+            'OCO',
+            'AEA'
+        ], {
+            A: 'extendedcrafting:advanced_component',
+            W: '#minecraft:wool',
+            O: 'kubejs:high_entropy_alloy_sheet',
+            C: 'create:fluid_tank',
+            E: 'extendedcrafting:enhanced_ender_catalyst'
+        }).id('finality:shetiphian/ender_tank')
+    }
+})
