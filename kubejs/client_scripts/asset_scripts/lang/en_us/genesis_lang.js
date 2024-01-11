a0e656276b03c40f7306d06bf70a74cf265e5716
CelestialAbyss <30322176+CelestialAbyss@users.noreply.github.com>
2024-01-11 02:14:19 -0500

    Even more restructuring...

--- kubejs/client_scripts/asset_scripts/lang/en_us/genesis_lang.js
+++ kubejs/client_scripts/asset_scripts/lang/en_us/genesis_lang.js
@@ -353,6 +353,9 @@ ClientEvents.lang('en_us', event => {
     if (Platform.isLoaded('roughlyenoughitems')) {
         event.add('roughlyenoughitems', 'text.rei.recipe_id', '\n%sRecipe ID: %s')
     }
+    if (Platform.isLoaded('aether')) {
+        event.add("aether", "gui.aether.pro_tip", "Aether Tip")
+    }
     if (Platform.isLoaded('alexsmobs')) {
         event.addAll('alexsmobs', {
             "alexsmobs.potion.soulsteal.desc": "Your attacks are granted life-steal.",
@@ -541,18 +544,7 @@ ClientEvents.lang('en_us', event => {
             "item.minecraft.bone_meal.tooltip.behaviour3": "_Grows additional_ Sea Pickles."
         })
     }
-    if (Platform.isLoaded('universalbonemeal')) {
-        event.addAll('universalbonemeal', {
-            "item.minecraft.bone_meal.tooltip": "BONE MEAL",
-            "item.minecraft.bone_meal.tooltip.summary": "Used for _accelerating_ the _growth_ of plants. However, it can now _universally_ bone meal _a lot more things_ compared to before!",
-            "item.minecraft.bone_meal.tooltip.condition1": "On right click on plants or crops:",
-            "item.minecraft.bone_meal.tooltip.behaviour1": "_Accelerates_ growth, self explanatory.",
-            "item.minecraft.bone_meal.tooltip.condition2": "On right click on grass blocks:",
-            "item.minecraft.bone_meal.tooltip.behaviour2": "Generates flowers and grass.",
-            "item.minecraft.bone_meal.tooltip.condition3": "On right click on Sea Pickles on living coral blocks:",
-            "item.minecraft.bone_meal.tooltip.behaviour3": "_Grows additional_ Sea Pickles."
-        })
-    }
+
     // deprecate in 1.21+
     if (Platform.isLoaded('craftingautomat')) {
         event.addAll('craftingautomat', {
