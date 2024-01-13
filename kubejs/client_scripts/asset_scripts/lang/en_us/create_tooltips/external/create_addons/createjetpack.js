12a8f07c57b71f14b69245737bf7890d90d26300
CelestialAbyss <30322176+CelestialAbyss@users.noreply.github.com>
2024-01-12 19:47:18 -0500

    Asset patching and lang edits.
    - Apparently the potion base transitional item texture was missing, it's implemented now.
    - Edited lang for Create Jetpack so it's clearer to players on how to use it and to reflect keybind changes.
    - Head Hunter enchantment description added.

---
+++ kubejs/client_scripts/asset_scripts/lang/en_us/create_tooltips/external/create_addons/createjetpack.js
@@ -0,0 +1,29 @@
+// requires: create_jetpack
+
+ClientEvents.lang('en_us', event => {
+    event.addAll('create_jetpack', {
+        "item.create_jetpack.jetpack.tooltip.summary": "Allows _levitation_ using pressurized air. Keybinds have been _changed_ to avoid keybind conflicts.",
+        "item.create_jetpack.jetpack.tooltip.condition1": "Adding pressurized air",
+        "item.create_jetpack.jetpack.tooltip.behaviour1": "Supply _rotational force_ through the top shaft connection.",
+        "item.create_jetpack.jetpack.tooltip.control1": "Press [JUMP]",
+        "item.create_jetpack.jetpack.tooltip.action1": "Fly upwards.",
+        "item.create_jetpack.jetpack.tooltip.control2": "Press [SHIFT]",
+        "item.create_jetpack.jetpack.tooltip.action2": "Fly downwards.",
+        "item.create_jetpack.jetpack.tooltip.control3": "Press [Ctrl] + [G]",
+        "item.create_jetpack.jetpack.tooltip.action3": "Turn jetpack on or off.",
+        "item.create_jetpack.jetpack.tooltip.control4": "Press [Ctrl] + [H]",
+        "item.create_jetpack.jetpack.tooltip.action4": "Toggles the _hovering_ function.",
+        // Netherite Jetpack
+        "item.create_jetpack.netherite_jetpack.tooltip.summary": "Allows _levitation_ using pressurized air. Keybinds have been _changed_ to avoid keybind conflicts.",
+        "item.create_jetpack.netherite_jetpack.tooltip.condition1": "Adding pressurized air",
+        "item.create_jetpack.netherite_jetpack.tooltip.behaviour1": "Supply _rotational force_ through the top shaft connection.",
+        "item.create_jetpack.netherite_jetpack.tooltip.control1": "Press [JUMP]",
+        "item.create_jetpack.netherite_jetpack.tooltip.action1": "Fly upwards",
+        "item.create_jetpack.netherite_jetpack.tooltip.control2": "Press [SHIFT]",
+        "item.create_jetpack.netherite_jetpack.tooltip.action2": "Fly downwards",
+        "item.create_jetpack.netherite_jetpack.tooltip.control3": "Press [Ctrl] + [G]",
+        "item.create_jetpack.netherite_jetpack.tooltip.action3": "Turn jetpack on or off.",
+        "item.create_jetpack.netherite_jetpack.tooltip.control4": "Press [Ctrl] + [H]",
+        "item.create_jetpack.netherite_jetpack.tooltip.action4": "Toggles the _hovering_ function."
+    })
+})
--- kubejs/client_scripts/asset_scripts/lang/en_us/external_lang/HeadHunter.js
+++ kubejs/client_scripts/asset_scripts/lang/en_us/external_lang/HeadHunter.js
@@ -6,7 +6,7 @@ ClientEvents.lang('en_us', event => {
         event.add(
             'headhunter_mod',
             'enchantment.headhunter_mod.hunters_brand_enchantment.desc',
-            'Information is not clear but here is an attempt... Critical hits apply a mark to the target, and damage gets stored for 5 seconds. Every hit also applies a random debuff.'
+            'Information is not clear but here is an attempt to summarize the information provided... Critical hits apply a mark to the target, and damage gets stored for 5 seconds. Every hit also applies a random debuff.'
         )
     }
 })
