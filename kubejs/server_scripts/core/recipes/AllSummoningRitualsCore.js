017971e4dc4927dc05bc9434b1d9adea7ed7acd5
CelestialAbyss <30322176+CelestialAbyss@users.noreply.github.com>
2024-01-24 09:06:58 -0500

    Insomniac adds butterfly summoning rituals
    - Forgot to commit this yesterday but it's 9AM now... I should sleep. May require some proper reviewing when I wake up.

--- kubejs/server_scripts/core/recipes/AllSummoningRitualsCore.js
+++ kubejs/server_scripts/core/recipes/AllSummoningRitualsCore.js
@@ -44,6 +44,167 @@ ServerEvents.recipes(event => {
         )
         .recipeTime(10)
         .id('finality:summoning/warden')
+
+    if (Platform.isLoaded('lilwings')) {
+
+        event.recipes.summoningrituals.altar('lilwings:paper_wings')
+            .input([
+                'minecraft:brown_mushroom',
+                'minecraft:mycelium'
+            ])
+            .mobOutput(
+                SummoningOutput.mob('lilwings:aponi_butterfly')
+                    .count(3)
+                    .offset(0, 3, 0)
+                    .spread(3, 0, 3)
+            )
+            .recipeTime(25)
+            .id('finality:summoning/aponi_butterfly')
+
+        event.recipes.summoningrituals.altar('lilwings:paper_wings')
+            .input([
+                'minecraft:dandelion',
+                'minecraft:milk_bucket'
+            ])
+            .mobOutput(
+                SummoningOutput.mob('lilwings:butter_gold_butterfly')
+                    .count(3)
+                    .offset(0, 3, 0)
+                    .spread(3, 0, 3)
+            )
+            .recipeTime(25)
+            .id('finality:summoning/butter_gold_butterfly')
+
+        event.recipes.summoningrituals.altar('lilwings:paper_wings')
+            .input([
+                'minecraft:slime_ball',
+                'minecraft:pink_dye'
+            ])
+            .mobOutput(
+                SummoningOutput.mob('lilwings:cloudy_puff_butterfly')
+                    .count(3)
+                    .offset(0, 3, 0)
+                    .spread(3, 0, 3)
+            )
+            .recipeTime(25)
+            .id('finality:summoning/cloudy_puff_butterfly')
+
+        event.recipes.summoningrituals.altar('lilwings:paper_wings')
+            .input([
+                '4x minecraft:amethyst_shard'
+            ])
+            .mobOutput(
+                SummoningOutput.mob('lilwings:crystal_puff_butterfly')
+                    .count(3)
+                    .offset(0, 3, 0)
+                    .spread(3, 0, 3)
+            )
+            .recipeTime(25)
+            .id('finality:summoning/crystal_puff_butterfly')
+
+        event.recipes.summoningrituals.altar('lilwings:paper_wings')
+            .input([
+                'minecraft:dripstone_block',
+                'minecraft:clay',
+                'minecraft:moss_block'
+            ])
+            .mobOutput(
+                SummoningOutput.mob('lilwings:grayling_butterfly')
+                    .count(3)
+                    .offset(0, 3, 0)
+                    .spread(3, 0, 3)
+            )
+            .recipeTime(25)
+            .id('finality:summoning/grayling_butterfly')
+
+        event.recipes.summoningrituals.altar('lilwings:paper_wings')
+            .input([
+                'minecraft:cocoa_beans',
+                'minecraft:beetroot'
+            ])
+            .mobOutput(
+                SummoningOutput.mob('lilwings:painted_panther_butterfly')
+                    .count(3)
+                    .offset(0, 3, 0)
+                    .spread(3, 0, 3)
+            )
+            .recipeTime(25)
+            .id('finality:summoning/painted_panther_butterfly')
+
+        event.recipes.summoningrituals.altar('lilwings:paper_wings')
+            .input([
+                'minecraft:apple',
+                Ingredient.of('#minecraft:leaves')
+            ])
+            .mobOutput(
+                SummoningOutput.mob('lilwings:red_applefly_butterfly')
+                    .count(3)
+                    .offset(0, 3, 0)
+                    .spread(3, 0, 3)
+            )
+            .recipeTime(25)
+            .id('finality:summoning/red_applefly_butterfly')
+
+        event.recipes.summoningrituals.altar('lilwings:paper_wings')
+            .input([
+                'minecraft:red_mushroom',
+                'minecraft:mycelium'
+            ])
+            .mobOutput(
+                SummoningOutput.mob('lilwings:shroom_skipper_butterfly')
+                    .count(3)
+                    .offset(0, 3, 0)
+                    .spread(3, 0, 3)
+            )
+            .recipeTime(25)
+            .id('finality:summoning/shroom_skipper_butterfly')
+
+        event.recipes.summoningrituals.altar('lilwings:paper_wings')
+            .input([
+                'minecraft:wheat_seeds',
+                'minecraft:pumpkin_seeds',
+                'minecraft:melon_seeds',
+                'minecraft:beetroot_seeds',
+                'minecraft:string'
+            ])
+            .mobOutput(
+                SummoningOutput.mob('lilwings:swallow_tail_butterfly')
+                    .count(3)
+                    .offset(0, 3, 0)
+                    .spread(3, 0, 3)
+            )
+            .recipeTime(25)
+            .id('finality:summoning/swallow_tail_butterfly')
+
+        event.recipes.summoningrituals.altar('lilwings:paper_wings')
+            .input([
+                'minecraft:glow_ink_sac',
+                'minecraft:lily_pad'
+            ])
+            .mobOutput(
+                SummoningOutput.mob('lilwings:swamp_hopper_butterfly')
+                    .count(3)
+                    .offset(0, 3, 0)
+                    .spread(3, 0, 3)
+            )
+            .recipeTime(25)
+            .id('finality:summoning/swamp_hopper_butterfly')
+
+        event.recipes.summoningrituals.altar('lilwings:paper_wings')
+            .input([
+                'minecraft:snowball',
+                'minecraft:ice'
+            ])
+            .mobOutput(
+                SummoningOutput.mob('lilwings:white_fox_butterfly')
+                    .count(3)
+                    .offset(0, 3, 0)
+                    .spread(3, 0, 3)
+            )
+            .recipeTime(25)
+            .id('finality:summoning/white_fox_butterfly')
+
+    }
 })
 
 /*
