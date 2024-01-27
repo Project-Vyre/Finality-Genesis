38a0c90ed67070d6485e025edd14db30337e6089
CelestialAbyss <30322176+CelestialAbyss@users.noreply.github.com>
2024-01-26 21:04:32 -0500

    Made the eternal meats from Artifacts saltable.

--- kubejs/server_scripts/handlers/mortuusars/salt.js
+++ kubejs/server_scripts/handlers/mortuusars/salt.js
@@ -7,3 +7,12 @@ ServerEvents.recipes(event => {
         Fluid.of('minecraft:water', 1000)
     ).heated().id('finality:create_salt_compat')
 })
+
+ServerEvents.tags('item', event => {
+    if (Platform.isLoaded('artifacts')) {
+        event.add('salt:can_be_salted', [
+            'artifacts:everlasting_beef',
+            'artifacts:eternal_steak'
+        ])
+    }
+})
