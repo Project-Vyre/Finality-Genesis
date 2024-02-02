fd5095b355bd455fe09de341ba9476f1a9712a88
CelestialAbyss <30322176+CelestialAbyss@users.noreply.github.com>
2024-02-01 21:30:52 -0500

    Eversor gem things
    - All the crit chance
    - Removed apotheosis.js custom lang keys. I have another solution.

--- kubejs/client_scripts/asset_scripts/lang/en_us/external_lang/apotheosis.js
+++
@@ -1,9 +0,0 @@
-// requires: apotheosis
-
-ClientEvents.lang('en_us', event => {
-    event.add(
-        'apotheosis',
-        'bonus.apotheosis:multi_attr.desc.and_and',
-        '%s and %s and %s'
-    )
-})
