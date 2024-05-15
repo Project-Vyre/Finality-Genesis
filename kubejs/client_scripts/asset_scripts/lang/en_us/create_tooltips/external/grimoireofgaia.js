bba82eb133181559c30c1da9a9b9008086cbcfb3
CelestialAbyss <30322176+CelestialAbyss@users.noreply.github.com>
2024-05-15 08:28:15 -0400

    Buffed GoG items and implemented Create tooltips.
    - Increased durability, attack damage and fire resistance wherever applicable.
    - Explained how the books are supposed to be used...

--- kubejs/client_scripts/asset_scripts/lang/en_us/create_tooltips/external/grimoireofgaia.js
+++ kubejs/client_scripts/asset_scripts/lang/en_us/create_tooltips/external/grimoireofgaia.js
@@ -49,4 +49,129 @@ ClientEvents.lang('en_us', event => {
     event.add('grimoireofgaia', 'block.grimoireofgaia.doll_maid.tooltip.condition2', 'Alternative Acquisition Method')
     event.add('grimoireofgaia', 'block.grimoireofgaia.doll_maid.tooltip.behaviour2', 'Can now be acquired from the Goblin Trader.')
   }
+  event.addAll(
+    'grimoireofgaia',
+    createTooltip('grimoireofgaia:weapon_book')
+      .addSummary('Upon holding this book you feel _weight_ when you give it momentum... Perhaps it is a weapon? Looks can be _deceptive_ after all.')
+      .build()
+  )
+  event.addAll(
+    'grimoireofgaia',
+    createTooltip('grimoireofgaia:weapon_book_freezing')
+      .addSummary('Upon holding this book you instantly feel the _cold aura_ around it and the air begins to sink as white fog.')
+      .addBehaviour([
+        'Usage',
+        'Upon hitting an entity with the book in your main hand, _Slowness II_ is applied to them.'
+      ])
+      .setPalette($Palette.BLUE)
+      .build()
+  )
+  event.addAll(
+    'grimoireofgaia',
+    createTooltip('grimoireofgaia:weapon_book_nightmare')
+      .addSummary('The book has a dark purple aura emanating from it.')
+      .addBehaviour([
+        'Usage',
+        'Upon hitting an entity with the book in your main hand, _Mining Fatigue II_ is applied to them.'
+      ])
+      .setPalette($Palette.PURPLE)
+      .build()
+  )
+  event.addAll(
+    'grimoireofgaia',
+    createTooltip('grimoireofgaia:weapon_book_metal')
+      .addSummary('Both the cover and pages appear to be composed of metal, and it appears hitting something is enough to apply some considerable force.')
+      .addBehaviour([
+        'Usage',
+        'Upon hitting an entity with the book in your main hand, _Nausea_ is applied to them.'
+      ])
+      .setPalette($Palette.GRAY_AND_WHITE)
+      .build()
+  )
+  event.addAll(
+    'grimoireofgaia',
+    createTooltip('grimoireofgaia:weapon_book_ender')
+      .addSummary('The book has a black aura emanating from it, devoid of light.')
+      .addBehaviour([
+        'Usage',
+        'Upon hitting an entity with the book in your main hand, _Blindness_ is applied to them.'
+      ])
+      .setPalette($Palette.PURPLE)
+      .build()
+  )
+  event.addAll(
+    'grimoireofgaia',
+    createTooltip('grimoireofgaia:weapon_book_hunger')
+      .addSummary('The book appears to be made of flesh through and through, the cover composed of a red fleshy skin.')
+      .addBehaviour([
+        'Usage',
+        'Upon hitting an entity with the book in your main hand, _Hunger_ is applied to them.'
+      ])
+      .setPalette($Palette.RED)
+      .build()
+  )
+  event.addAll(
+    'grimoireofgaia',
+    createTooltip('grimoireofgaia:weapon_book_battle')
+      .addSummary('The book transfers combat knowledge to you and you suddenly feel like a master of the ages.')
+      .addBehaviour([
+        'Usage',
+        'Upon hitting an entity with the book in your main hand, _Weakness II_ is applied to them.'
+      ])
+      .build()
+  )
+  event.addAll(
+    'grimoireofgaia',
+    createTooltip('grimoireofgaia:weapon_book_nature')
+      .addSummary('The cover is rife with leaves, with a small red flower.')
+      .addBehaviour([
+        'Usage',
+        'Upon hitting an entity with the book in your main hand, _Poison II_ is applied to them.'
+      ])
+      .setPalette($Palette.GREEN)
+      .build()
+  )
+  event.addAll(
+    'grimoireofgaia',
+    createTooltip('grimoireofgaia:weapon_book_wither')
+      .addSummary('The cover is made up of wither rib bones, the pages made up of black smoke.')
+      .addBehaviour([
+        'Usage',
+        'Upon hitting an entity with the book in your main hand, _Withering_ is applied to them.'
+      ])
+      .setPalette($Palette.GRAY)
+      .build()
+  )
+  event.addAll(
+    'grimoireofgaia',
+    createTooltip('grimoireofgaia:weapon_book_buff')
+      .addSummary('You feel empowered by combat knowledge...')
+      .addBehaviour([
+        'Usage',
+        'Upon hitting a friend with the book in your main hand, _Strength_, _Resistance_ and _Regeneration_ are applied to them.'
+      ])
+      .setPalette($Palette.RED)
+      .build()
+  )
+  event.addAll(
+    'grimoireofgaia',
+    createTooltip('grimoireofgaia:minotaur_hammer')
+      .addSummary('A _devastating_ weapon once wielded by the _Minotaur_.')
+      .setPalette($Palette.RED)
+      .build()
+  )
+  event.addAll(
+    'grimoireofgaia',
+    createTooltip('grimoireofgaia:cursed_metal_sword')
+      .addSummary('A cursed sword once wielded by the _Dullahan_.')
+      .setPalette($Palette.PURPLE)
+      .build()
+  )
+  event.addAll(
+    'grimoireofgaia',
+    createTooltip('grimoireofgaia:metal_club')
+      .addSummary('Once wielded by the _Oni_.')
+      .setPalette($Palette.RED)
+      .build()
+  )
 })
--- kubejs/startup_scripts/modification/item_modification/GrimoireOfGaiaItemModification.js
+++ kubejs/startup_scripts/modification/item_modification/GrimoireOfGaiaItemModification.js
@@ -1,6 +1,25 @@
 // requires: grimoireofgaia
 
 ItemEvents.modification(event => {
+  let gogBooks = [
+    'weapon_book',
+    'weapon_book_buff',
+    'weapon_book_freezing',
+    'weapon_book_nightmare',
+    'weapon_book_metal',
+    'weapon_book_ender',
+    'weapon_book_hunger',
+    'weapon_book_battle',
+    'weapon_book_nature',
+    'weapon_book_wither'
+  ]
+  for (let i = 0; i < gogBooks.length; i++) {
+    let book = gogBooks[i];
+    event.modify(`grimoireofgaia:${book}`, item => {
+      item.maxDamage = -1 // Previously 1024
+      item.fireResistant = true
+    })
+  }
   let weaponBooks = [
     'weapon_book',
     'weapon_book_freezing',
@@ -15,8 +34,33 @@ ItemEvents.modification(event => {
   for (let i = 0; i < weaponBooks.length; i++) {
     let book = weaponBooks[i];
     event.modify(`grimoireofgaia:${book}`, item => {
-      item.maxDamage = 1024
-      item.fireResistant = true
+      item.attackDamage = 10
     })
   }
+  let weaponFans = [
+    'fan',
+    'fan_fire',
+    'fan_ice'
+  ]
+  for (let i = 0; i < weaponFans.length; i++) {
+    let fans = weaponFans[i];
+    event.modify('grimoireofgaia:' + fans, item => {
+      item.maxDamage = 1024
+    })
+  }
+  event.modify('grimoireofgaia:metal_club', item => {
+    item.maxDamage = 1024
+    item.attackDamage = 17
+    item.fireResistant = true
+  })
+  event.modify('grimoireofgaia:cursed_metal_sword', item => {
+    item.maxDamage = 1024
+    item.attackDamage = 11
+    item.fireResistant = true
+  })
+  event.modify('grimoireofgaia:minotaur_hammer', item => {
+    item.maxDamage = 1024
+    item.attackDamage = 24
+    item.fireResistant = true
+  })
 })
