dc979efa1f6dd7848186cb127dbc313539f5cfed
CelestialAbyss <30322176+CelestialAbyss@users.noreply.github.com>
2024-05-24 17:36:55 -0400

    Moved mod_rename.js to modification folder

---
+++ kubejs/startup_scripts/modification/mod_rename.js
@@ -0,0 +1,7 @@
+if (Platform.isLoaded('craftingautomat')) {
+  Platform.setModName('craftingautomat', 'Minecraft')
+}
+
+if (Platform.isLoaded('headhunter_mod')) {
+  Platform.setModName('headhunter_mod', 'Head Hunter')
+}
