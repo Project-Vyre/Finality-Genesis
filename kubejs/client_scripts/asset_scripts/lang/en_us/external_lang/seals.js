cd2634f127dfcbbd2c8cbad39cc13150b4209369
CelestialAbyss <30322176+CelestialAbyss@users.noreply.github.com>
2024-05-31 07:17:22 -0400

    Forgot to include Seals title gen

---
+++ kubejs/client_scripts/asset_scripts/lang/en_us/external_lang/seals.js
@@ -0,0 +1,11 @@
+/**
+ * @file Generates lang for use in the Seals mod.
+ * @author CelestialAbyss <https://github.com/CelestialAbyss> Modpack lead
+ */
+
+// requires: seals
+
+ClientEvents.lang('en_us', event => {
+  event.add('kubejs', 'kubejs.seal.engineer', 'Engineer')
+  event.add('kubejs', 'kubejs.seal.conductor', 'Conductor')
+})
