9b5fea85b3ad571cbe9d948ffc9a3f0604cd6e90
CelestialAbyss <30322176+CelestialAbyss@users.noreply.github.com>
2024-06-11 07:41:50 -0400

    Updated en_us lang generation
    - Added the new music disc desc keys.

--- kubejs/client_scripts/asset_scripts/lang/en_us/genesis_lang.js
+++ kubejs/client_scripts/asset_scripts/lang/en_us/genesis_lang.js
@@ -345,17 +345,24 @@ ClientEvents.lang('en_us', event => {
   })
   let discLang = {
     'arlecchino_battle_theme': 'Farabi Hasan Music - Arlecchino Battle Theme Phase 1 & 2 (Fan-Made) | Genshin Impact',
+    'bad_deeds': 'Cjbeards - Bad Deeds',
     'bad_deeds_inst': 'Cjbeards - Bad Deeds (Instrumental)',
+    'black_white_red': 'Cjbeards - Black White Red',
+    'black_white_red_inst': 'Cjbeards - Black White Red (Instrumental)',
     'boo_hoo': 'Cjbeards - Boo Hoo',
     'columbina_battle_theme': 'Farabi Hasan Music - Columbina Battle Phase 1 & 2 (Fan-Made) | Genshin Impact',
     'from_the_shadows': 'Cjbeards - From the Shadows',
+    'get_away': 'Cjbeards - Get Away',
     'get_away_inst': 'Cjbeards - Get Away (Instrumental)',
     'heart_of_the_wicked': 'Cjbeards - Heart of the Wicked',
+    'mirror_mirror': 'Cjbeards - Mirror Mirror',
     'mirror_mirror_inst': 'Cjbeards - Mirror Mirror (Instrumental)',
+    'open_up': 'Cjbeards - Open Up',
     'open_up_inst': 'Cjbeards - Open Up (Instrumental)',
     'pantalone_battle_theme': 'Farabi Hasan Music - Pantalone Double Phase Battle Theme (Fan-Made) | Genshin Impact',
     'sandrone_battle_theme': 'Farabi Hasan Music - Sandrone Marionette Double Phase Battle Theme (Fan-Made) | Genshin Impact',
     'silent_night': 'Cjbeards - Silent Night',
+    'worst_nightmare': 'Cjbeards - Worst Nightmare',
     'worst_nightmare_inst': 'Cjbeards - Worst Nightmare (Instrumental)'
   }
   for (let [id, txt] of Object.entries(discLang)) {
