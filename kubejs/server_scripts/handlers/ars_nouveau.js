6770304b14f69ee8d4adba9f7ee69fd87f206ba1
CelestialAbyss <30322176+CelestialAbyss@users.noreply.github.com>
2024-06-13 11:45:38 -0400

    Ars Nouveau recipes initial commit

---
+++ kubejs/server_scripts/handlers/ars_nouveau.js
@@ -0,0 +1,67 @@
+
+/** 
+ * @file Ars Nouveau server handler
+ * @version 1.20.1.g
+ * @author CelestialAbyss <https://github.com/CelestialAbyss> Modpack lead
+ * @author Ryanite <> Modpack tester
+ * @author wierdslo <> Modpack tester and Ars Nouveau wizard
+ */
+
+// requires: ars_nouveau
+// requires: kubejs_create
+// ignored: false
+
+ServerEvents.recipes(event => {
+  event.remove([
+      { id: 'ars_nouveau:novice_spell_book' },
+      { id: 'ars_nouveau:apprentice_spell_book_upgrade' },
+      { id: 'ars_nouveau:archmage_spell_book_upgrade' }
+  ])
+  if (!Platform.isLoaded('aether')) {
+    event.shapeless('ars_nouveau:novice_spell_book', [
+        'minecraft:book',
+        'minecraft:diamond_shovel',
+        'minecraft:diamond_pickaxe',
+        'minecraft:diamond_axe',
+        'minecraft:diamond_sword'
+    ]).id('kubejs:fallback_novice_spell_book')
+  }
+  if (Platform.isLoaded('aether')) {
+    event.shapeless('ars_nouveau:novice_spell_book', [
+        'minecraft:book',
+        'aether:gravitite_shovel',
+        'aether:gravitite_pickaxe',
+        'aether:gravitite_axe',
+        'aether:gravitite_sword',
+        'minecraft:amethyst_shard',
+        'create:andesite_alloy',
+        'minecraft:golden_carrot'
+    ]).id('kubejs:novice_spell_book_from_zanite')
+  }
+  event.shapeless('ars_nouveau:apprentice_spell_book', [
+      'ars_nouveau:novice_spell_book',
+      'minecraft:crying_obsidian',
+      'create:rose_quartz',
+      'create:rose_quartz',
+      'create:rose_quartz',
+      'minecraft:glowstone',
+      'minecraft:glowstone',
+      'create:blaze_cake',
+      'create:blaze_cake'
+  ]).id('kubejs:apprentice_spell_book_upgrade')
+  event.shapeless('ars_nouveau:archmage_spell_book', [
+      'ars_nouveau:apprentice_spell_book',
+      'minecraft:nether_star',
+      'ars_nouveau:wilden_tribute',
+      'kubejs:null_matter',
+      'kubejs:null_matter',
+      'create:precision_mechanism',
+      'create:precision_mechanism',
+      'create:precision_mechanism',
+      'minecraft:totem_of_undying'
+  ]).id('kubejs:archmage_spell_book_upgrade')
+  event.recipes.create.milling([
+    '4x ars_nouveau:magebloom_fiber',
+    Item.of('ars_nouveau:magebloom_fiber', 4).withChance(0.12)
+  ], 'ars_noveau:magebloom').processingTime(150).id('kubejs:milling/compat/ars_nouveau/magebloom_fiber')
+})
