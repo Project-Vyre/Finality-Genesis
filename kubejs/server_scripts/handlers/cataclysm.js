965f3e2e15087facd150d0d5859299b324edd38d
CelestialAbyss <30322176+CelestialAbyss@users.noreply.github.com>
2024-06-14 08:52:17 -0400

    Ignited Revenant summoning ritual

--- kubejs/server_scripts/handlers/cataclysm.js
+++ kubejs/server_scripts/handlers/cataclysm.js
@@ -1,9 +1,12 @@
-// requires: cataclysm
-
 /**
  * @file Server handler for Cataclysm.
  * @author CelestialAbyss <https://github.com/CelestialAbyss> Modpack lead
  */
+
+// requires: cataclysm
+// requires: create
+// requires: lootjs
+// requires: summoningrituals
 
 ServerEvents.recipes(event => {
   event.shaped('cataclysm:athame', [
@@ -20,4 +23,25 @@ ServerEvents.recipes(event => {
     'minecraft:stone',
     'minecraft:sea_pickle'
   ]).id('finality:cataclysm/crystallized_coral_fragments')
+  event.recipes.summoningrituals.altar('minecraft:blaze_powder')
+    .input([
+      '4x minecraft:soul_sand',
+      '2x minecraft:bone_block',
+      '4x create:scorchia'
+    ])
+    .mobOutput(
+      SummoningOutput.mob('cataclysm:ignited_revenant')
+        .offset(0, 3, 0)
+        .spread(3, 0, 3)
+    )
+    .recipeTime(10)
+    .id('finality:summoning/ignited_revenant')
 })
+
+LootJS.modifiers(event => {
+  event.addEntityLootModifier('cataclysm:netherite_monstrosity')
+    .killedByPlayer().apply(ctx => {
+      ctx.addLoot('3x minecraft:netherite_block')
+    })
+})
+
