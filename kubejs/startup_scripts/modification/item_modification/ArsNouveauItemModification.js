6be82241765f74bc7ee6c0ed9b9961796b554121
CelestialAbyss <30322176+CelestialAbyss@users.noreply.github.com>
2024-06-14 21:00:05 -0400

    Make Ars Nouveau battlemage armor unbreakable

---
+++ kubejs/startup_scripts/modification/item_modification/ArsNouveauItemModification.js
@@ -0,0 +1,20 @@
+// requires: ars_nouveau
+
+let battle_mage = [
+  'ars_nouveau:battlemage_boots',
+  'ars_nouveau:battlemage_leggings',
+  'ars_nouveau:battlemage_robes',
+  'ars_nouveau:battlemage_hood'
+]
+
+ItemEvents.modification(event => {
+  event.modify('ars_nouveau:enchanters_sword', item => {
+    item.maxDamage = -1
+  })
+  for (let i = 0; i < battle_mage.length; i++) {
+    let element = battle_mage[i];
+    event.modify(element, item => {
+      item.maxDamage = -1
+    })
+  }
+})
