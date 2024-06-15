7ae8696ed2fc6edbfe495a807e2afa04785137aa
CelestialAbyss <30322176+CelestialAbyss@users.noreply.github.com>
2024-06-15 08:28:14 -0400

    Started to replace Reliquary's tooltips
    - Handcannon ammo Create tooltips added

---
+++ kubejs/client_scripts/asset_scripts/lang/en_us/create_tooltips/external/P3pp3rF1y/reliquary.js
@@ -0,0 +1,89 @@
+// requires: reliquary
+// requires: create
+
+ClientEvents.lang('en_us', event => {
+  event.addAll(
+    'kubejs',
+    createTooltip('reliquary:bullets/exorcism_bullet')
+      .addSummary('Highly effective against the _Undead_.')
+      .setPalette($Palette.RED)
+      .build()
+  )
+  event.addAll(
+    'kubejs',
+    createTooltip('reliquary:bullets/blaze_bullet')
+      .addSummary('Deals only _fire_ damage.')
+      .addBehaviour([
+        'Additional Info',
+        'Obviously does _not_ work against Blazes or other mobs made of the fire element.'
+      ])
+      .setPalette($Palette.YELLOW)
+      .build()
+  )
+  event.addAll(
+    'kubejs',
+    createTooltip('reliquary:bullets/ender_bullet')
+      .addSummary('Pierces _through_ enemies in addition to _seeking_ targets.')
+      .addBehaviour([
+        'Additional Info',
+        '_Faster_ projectile speed compared to Seeker Shots and _tighter_ tracking.'
+      ])
+      .setPalette($Palette.PURPLE)
+      .build()
+  )
+  event.addAll(
+    'kubejs',
+    createTooltip('reliquary:bullets/concussive_bullet')
+      .addSummary('Small AoE damage.')
+      .addBehaviour([
+        'Additional Info',
+        'Also _destroys_ blocks upon contact.'
+      ])
+      .setPalette($Palette.GREEN)
+      .build()
+  )
+  event.addAll(
+    'kubejs',
+    createTooltip('reliquary:bullets/buster_bullet')
+      .addSummary('Large AoE damage.')
+      .addBehaviour([
+        'Additional Info',
+        'Also _destroys_ blocks upon contact.'
+      ])
+      .setPalette($Palette.GREEN)
+      .build()
+  )
+  event.addAll(
+    'kubejs',
+    createTooltip('reliquary:bullets/seeker_bullet')
+      .addSummary('Seeks targets.')
+      .addBehaviour([
+        'Additional Info',
+        '_Slow_ projectile speed and loose tracking compared to _Ender Shots_.'
+      ])
+      .setPalette($Palette.BLUE)
+      .build()
+  )
+  event.addAll(
+    'kubejs',
+    createTooltip('reliquary:bullets/sand_bullet')
+      .addSummary('_Blinds_ targets.')
+      .addBehaviour([
+        'Additional Info',
+        'Also _irritates_ Creepers.'
+      ])
+      .setPalette($Palette.YELLOW)
+      .build()
+  )
+  event.addAll(
+    'kubejs',
+    createTooltip('reliquary:bullets/storm_bullet')
+      .addSummary('Stronger during _thunder storms_.')
+      .addBehaviour([
+        'Additional Info',
+        'Also _charges_ Creepers... I\'ll leave you to deal with that.'
+      ])
+      .setPalette($Palette.BLUE)
+      .build()
+  )
+})
---
+++ kubejs/client_scripts/tooltips_handler/tooltip_removal/mod_tooltips/reliquary.js
@@ -0,0 +1,21 @@
+// requires: reliquary
+
+let reliquary_shots = [
+  'reliquary:bullets/exorcism_bullet',
+  'reliquary:bullets/blaze_bullet',
+  'reliquary:bullets/ender_bullet',
+  'reliquary:bullets/concussive_bullet',
+  'reliquary:bullets/buster_bullet',
+  'reliquary:bullets/seeker_bullet',
+  'reliquary:bullets/sand_bullet',
+  'reliquary:bullets/storm_bullet'
+]
+
+ItemEvents.tooltip(event => {
+  for (let i = 0; i < reliquary_shots.length; i++) {
+    let element = reliquary_shots[i];
+    event.addAdvanced(element, (item, advanced, text) => {
+      text.remove(1)
+    })
+  }
+})
