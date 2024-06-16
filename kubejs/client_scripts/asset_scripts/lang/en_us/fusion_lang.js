2da9f9efb69d52c455f4ff313dd39d99e4b1b2d8
CelestialAbyss <30322176+CelestialAbyss@users.noreply.github.com>
2024-06-16 12:53:34 -0400

    Forgot Fusion related lang and tooltips from 1.19.2
    - Updated fusion_lang.js
    - Added fusion_tooltips.js because I forgot about it

--- kubejs/client_scripts/asset_scripts/lang/en_us/fusion_lang.js
+++ kubejs/client_scripts/asset_scripts/lang/en_us/fusion_lang.js
@@ -1,23 +1,25 @@
 // requires: fusion
 
 ClientEvents.lang('en_us', event => {
-  event.add(
-    'kubejs',
-    'block.kubejs.iridium_block_connecting',
-    'Block of Iridium (Connecting)'
-  )
+  event.addAll('kubejs', {
+    "block.kubejs.iridium_block_connecting": "Block of Iridium",
+    "block.kubejs.ornate_iridium_block_connecting": "Ornate Iridium Block",
+    "block.kubejs.iridium_quartz_block_connecting": "Iridium Quartz Block",
+    "block.kubejs.ornate_iridium_pillar_connecting": "Ornate Iridium Pillar",
+    "block.kubejs.iridium_tiles_connecting": "Iridium Tiles"
+  })
   if (Platform.isLoaded('textanimator')) {
     event.add(
       'kubejs',
       'block.kubejs.high_entropy_alloy_block_connecting',
-      '<rainb>Block of High Entropy Alloy (Connecting)</rainb>'
+      '<rainb>Block of High Entropy Alloy</rainb>'
     )
   }
   if (!Platform.isLoaded('textanimator')) {
     event.add(
       'kubejs',
       'block.kubejs.high_entropy_alloy_block_connecting',
-      'Block of High Entropy Alloy (Connecting)'
+      'Block of High Entropy Alloy'
     )
   }
 })
