c0ceb972217029c040f3050e76d2cc7a5f1cd123
CelestialAbyss <30322176+CelestialAbyss@users.noreply.github.com>
2024-06-22 18:55:39 -0400

    Added a Create tooltip for the Heart of Diamond

--- kubejs/client_scripts/src/lang/en_us/create_tooltips/external/quark.js
+++ kubejs/client_scripts/src/lang/en_us/create_tooltips/external/quark.js
@@ -12,4 +12,19 @@ ClientEvents.lang('en_us', event => {
     "block.quark.crate.tooltip": "CRATE",
     "block.quark.crate.tooltip.summary": "Can store a total of 640 _unique_ items. Good for storing items with _many_ variations. Not so good for storing items that are the same _and_ stack."
   })
+  event.addAll(
+    'quark',
+    createTooltip('quark:diamond_heart')
+      .addSummary('A twinkling heart of diamond, the eye within it staring back at you in curiosity.')
+      .addBehaviour([
+        'Acquisition',
+        'Can be acquired by _breaking_ a Stoneling\'s shell with a _light_ punch. Don\'t worry! The Stoneling is fine. The body is just a shell around its heart.'
+      ])
+      .addBehaviour([
+        'Additional Info',
+        'When a Stone block is _right clicked_, the Stoneling will construct a _new_ body out of it. It will also be more _comfortable_ with players after its ride in your inventory.'
+      ])
+      .setPalette($Palette.BLUE)
+      .build()
+  )
 })
