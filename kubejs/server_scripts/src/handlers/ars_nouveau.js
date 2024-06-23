ca4ad5985dd27b4f8a3041f59b0e97daf6f976d3
CelestialAbyss <30322176+CelestialAbyss@users.noreply.github.com>
2024-06-23 10:23:12 -0400

    Fixed issue in ars_nouveau.js
    - Sleepy me did not see the duplicate recipe ID.
    - Fixed weird formatting

--- kubejs/server_scripts/src/handlers/ars_nouveau.js
+++ kubejs/server_scripts/src/handlers/ars_nouveau.js
@@ -13,29 +13,29 @@
 
 ServerEvents.recipes(event => {
   event.remove([
-      { id: 'ars_nouveau:novice_spell_book' },
-      { id: 'ars_nouveau:apprentice_spell_book_upgrade' },
-      { id: 'ars_nouveau:archmage_spell_book_upgrade' }
+    { id: 'ars_nouveau:novice_spell_book' },
+    { id: 'ars_nouveau:apprentice_spell_book_upgrade' },
+    { id: 'ars_nouveau:archmage_spell_book_upgrade' }
   ])
   if (!Platform.isLoaded('aether')) {
     event.shapeless('ars_nouveau:novice_spell_book', [
-        'minecraft:book',
-        'minecraft:diamond_shovel',
-        'minecraft:diamond_pickaxe',
-        'minecraft:diamond_axe',
-        'minecraft:diamond_sword'
+      'minecraft:book',
+      'minecraft:diamond_shovel',
+      'minecraft:diamond_pickaxe',
+      'minecraft:diamond_axe',
+      'minecraft:diamond_sword'
     ]).id('kubejs:fallback_novice_spell_book')
   }
   if (Platform.isLoaded('aether')) {
     event.shapeless('ars_nouveau:novice_spell_book', [
-        'minecraft:book',
-        'aether:zanite_shovel',
-        'aether:zanite_pickaxe',
-        'aether:zanite_axe',
-        'aether:zanite_sword',
-        'minecraft:amethyst_shard',
-        'create:andesite_alloy',
-        'minecraft:golden_carrot'
+      'minecraft:book',
+      'aether:zanite_shovel',
+      'aether:zanite_pickaxe',
+      'aether:zanite_axe',
+      'aether:zanite_sword',
+      'minecraft:amethyst_shard',
+      'create:andesite_alloy',
+      'minecraft:golden_carrot'
     ]).id('kubejs:novice_spell_book_from_zanite')
   }
   if (Platform.isLoaded('malum')) {
@@ -48,29 +48,29 @@ ServerEvents.recipes(event => {
       'minecraft:amethyst_shard',
       'create:andesite_alloy',
       'minecraft:golden_carrot'
-  ]).id('kubejs:novice_spell_book_from_zanite')
+    ]).id('kubejs:novice_spell_book_from_soul_stained_steel')
   }
   event.shapeless('ars_nouveau:apprentice_spell_book', [
-      'ars_nouveau:novice_spell_book',
-      'minecraft:crying_obsidian',
-      'create:rose_quartz',
-      'create:rose_quartz',
-      'create:rose_quartz',
-      'minecraft:glowstone',
-      'minecraft:glowstone',
-      'create:blaze_cake',
-      'create:blaze_cake'
+    'ars_nouveau:novice_spell_book',
+    'minecraft:crying_obsidian',
+    'create:rose_quartz',
+    'create:rose_quartz',
+    'create:rose_quartz',
+    'minecraft:glowstone',
+    'minecraft:glowstone',
+    'create:blaze_cake',
+    'create:blaze_cake'
   ]).id('kubejs:apprentice_spell_book_upgrade')
   event.shapeless('ars_nouveau:archmage_spell_book', [
-      'ars_nouveau:apprentice_spell_book',
-      'minecraft:nether_star',
-      'ars_nouveau:wilden_tribute',
-      'kubejs:null_matter',
-      'kubejs:null_matter',
-      'create:precision_mechanism',
-      'create:precision_mechanism',
-      'create:precision_mechanism',
-      'minecraft:totem_of_undying'
+    'ars_nouveau:apprentice_spell_book',
+    'minecraft:nether_star',
+    'ars_nouveau:wilden_tribute',
+    'kubejs:null_matter',
+    'kubejs:null_matter',
+    'create:precision_mechanism',
+    'create:precision_mechanism',
+    'create:precision_mechanism',
+    'minecraft:totem_of_undying'
   ]).id('kubejs:archmage_spell_book_upgrade')
   event.recipes.create.milling([
     '4x ars_nouveau:magebloom_fiber',
