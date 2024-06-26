45e09599cdf6795c162b9cc7ff9baf089fc8cd8e
CelestialAbyss <30322176+CelestialAbyss@users.noreply.github.com>
2024-06-26 16:00:38 -0400

    Work in progress recipe schemas

--- kubejs/startup_scripts/src/@recipes/createaddition.js
+++ kubejs/startup_scripts/src/@recipes/createaddition.js
@@ -3,6 +3,7 @@
  * @author ChiefArug <https://github.com/ChiefArug> Original script author
  */
 
+// requires: createaddition
 // ignored: true
 
 const $RecipeSchema = Java.loadClass('dev.latvian.mods.kubejs.recipe.schema.RecipeSchema')
@@ -10,29 +11,25 @@ const $RecipeComponentBuilder = Java.loadClass('dev.latvian.mods.kubejs.recipe.c
 
 StartupEvents.recipeSchemaRegistry(event => {
   const Components = event.components
-  const result = Components.get('outputItem')().key('result')
-  const input = Components.get('inputItem')().key('input')
-
-  const fluidOrTagInput = Components.get('inputFluid')().or(
-    new $RecipeComponentBuilder(2)
-      .add(Components.get('tag')({ registry: 'fluid' }).key('fluidTag'))
-      .add(Components.get('intNumber')().key('amount'))
-      .inputRole()).key('input')
-
 
   event.register('createaddition:rolling', new $RecipeSchema(
-    result,
-    input
+    Components.get('outputItem')().key('result'),
+    Components.get('inputItem')().key('input')
   ))
   event.register('createaddition:charging', new $RecipeSchema(
-    result,
-    input,
+    Components.get('outputItem')().key('result'),
+    Components.get('inputItem')().key('input'),
     Components.get('intNumber')().key('energy'),
     Components.get('intNumber')().key('maxChargeRate').optional(2147483647)
   ))
   event.register('createaddition:liquid_burning', new $RecipeSchema(
     Components.get('intNumber')().key('burnTime'),
-    fluidOrTagInput,
+    Components.get('inputFluid')().or(
+      new $RecipeComponentBuilder(2)
+        .add(Components.get('tag')({ registry: 'fluid' }).key('fluidTag'))
+        .add(Components.get('intNumber')().key('amount'))
+        .inputRole()
+    ).key('input'),
     Components.get('bool')().key('superheated').optional(false)
   ))
 })
---
+++ kubejs/startup_scripts/src/@recipes/farmersdelight.js
@@ -0,0 +1,33 @@
+/**
+ * @file Recipe schema for Farmer's Delight.
+ */
+
+// requires: farmersdelight
+// ignored: true
+
+const $RecipeSchema = Java.loadClass('dev.latvian.mods.kubejs.recipe.schema.RecipeSchema')
+const $RecipeComponentBuilder = Java.loadClass('dev.latvian.mods.kubejs.recipe.component.RecipeComponentBuilder')
+
+StartupEvents.recipeSchemaRegistry(event => {
+  const Components = event.components
+
+  event.register('farmersdelight:cutting', new $RecipeSchema(
+    Components.get('outputItemArray')().key('result'),
+    Components.get('inputItemArray')().key('ingredients'),
+    Components.get('inputItem')().or(
+      new $RecipeComponentBuilder(2)
+        .add(Components.get('nonBlankString')().key('type'))
+        .add(Components.get('nonBlankString')().key('action'))
+        .inputRole()
+    ).key('tool'),
+    Components.get('nonBlankString')().key('sound').defaultOptional()
+  ))
+  event.register('farmersdelight:cooking', new $RecipeSchema(
+    Components.get('outputItem')().key('result'),
+    Components.get('inputItemArray')().key('ingredients'),
+    Components.get('inputItem')().key('container').defaultOptional(),
+    Components.get('intNumber')().key('cookingtime').optional(200),
+    Components.get('floatNumber')().key('experience').optional(1.0),
+    Components.get('nonBlankString')().key('recipe_book_tab').defaultOptional()
+  ))
+})
