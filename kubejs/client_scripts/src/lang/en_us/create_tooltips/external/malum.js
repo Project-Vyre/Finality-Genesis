b6535fcde4ef261b93c88457e743ad3fe08e29b7
CelestialAbyss <30322176+CelestialAbyss@users.noreply.github.com>
2024-07-07 01:48:39 -0400

    Updated Create tooltips for Malum
    - Added Create tooltips for Blazing Quartz, Chtonic Gold Ore and Cthonic Gold

--- kubejs/client_scripts/src/lang/en_us/create_tooltips/external/malum.js
+++ kubejs/client_scripts/src/lang/en_us/create_tooltips/external/malum.js
@@ -24,8 +24,7 @@ ClientEvents.lang('en_us', event => {
       .setPalette($Palette.PURPLE)
       .build()
   )
-  event.addAll(
-    'kubejs',
+  event.addAll('malum',
     createTooltip('malum:raw_soulstone')
       .addSummary('An _essential_ material for the dark magics in Malum.')
       .addBehaviour([
@@ -35,6 +34,47 @@ ClientEvents.lang('en_us', event => {
       .setPalette($Palette.PURPLE)
       .build()
   )
+  event.addAll(
+    'malum',
+    createTooltip('malum:blazing_quartz')
+      .addSummary('You can feel the heat within. Can be utilized as a fuel.')
+      .addBehaviour([
+        'Renewal',
+        'Utilize an _Alchemical Impetus_ with 2x Infernal Spirit and 2x Arcane Spirit in a _Spirit Crucible_.'
+      ])
+      .setPalette($Palette.YELLOW)
+      .build()
+  )
+  event.addAll(
+    'malum',
+    createTooltip('malum:cthonic_gold_ore')
+      .addSummary('A very rare ore that can be crushed to produce other metals.')
+      .addBehaviour([
+        'Additional Info',
+        'There are only a finite number of these in each world.'
+      ])
+      .setPalette($Palette.YELLOW)
+      .build()
+  )
+  event.addAll(
+    'malum',
+    createTooltip('malum:cthonic_gold')
+      .addSummary('A very rare ore that can be crushed to produce other metals.')
+      .addBehaviour([
+        'Additional Info',
+        'There are only a finite number of these in each world.'
+      ])
+      .setPalette($Palette.YELLOW)
+      .build()
+  )
+  if (Platform.isLoaded('create_mechanical_extruder')) {
+    event.addAll('malum', {
+      "block.malum.cthonic_gold_ore.tooltip.condition2": "Renewal",
+      "block.malum.cthonic_gold_ore.tooltip.behaviour2": "Can be _renewed_ with the Mechanical Extruder. Right click in the recipe viewer to see details.",
+      "item.malum.cthonic_gold.tooltip.condition2": "Renewal",
+      "item.malum.cthonic_gold.tooltip.behaviour2": "Can be _renewed_ with the Mechanical Extruder. Right click the _block form_ to see details as the recipe does not show itself reliably."
+    })
+  }
   /*
   event.addAll('malum',
       createTooltip('malum:runewood_sign')
