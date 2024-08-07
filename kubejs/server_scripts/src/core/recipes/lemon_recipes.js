5dfd08243355e7e3f688131a2e98a394f1eea020
CelestialAbyss <30322176+CelestialAbyss@users.noreply.github.com>
2024-08-06 23:29:33 -0400

    Lemon seeds can now be composted like other seeds.

--- kubejs/server_scripts/src/core/recipes/lemon_recipes.js
+++ kubejs/server_scripts/src/core/recipes/lemon_recipes.js
@@ -5,19 +5,23 @@ ServerEvents.recipes(event => {
   event.recipes.create.cutting(
     '4x kubejs:lemon_slice',
     'kubejs:lemon'
-  ).processingTime(25).id('finality:lemon_slicing_with_saw')
+  ).processingTime(25).id('kubejs:lemon_slicing_with_saw')
   event.recipes.create.compacting([
     Fluid.of('kubejs:lemon_juice', 100),
     'kubejs:lemon_seed'
-  ], 'kubejs:lemon').id('finality:whole_lemon_squeezing')
+  ], 'kubejs:lemon').id('kubejs:whole_lemon_squeezing')
   event.recipes.create.compacting([
     Fluid.of('kubejs:lemon_juice', 25),
     'kubejs:lemon_seed'
-  ], 'kubejs:lemon_slice').id('finality:lemon_slice_squeezing')
+  ], 'kubejs:lemon_slice').id('kubejs:lemon_slice_squeezing')
   event.custom({
     type: 'farmersdelight:cutting',
     ingredients: [Ingredient.of('kubejs:lemon').toJson()],
     result: [Item.of('kubejs:lemon_slice', 4).toJson()],
     tool: Ingredient.of('#forge:tools/knives').toJson()
-  }).id('finality:cutting/lemon_slice')
+  }).id('kubejs:cutting/lemon_slice')
+})
+
+ServerEvents.compostableRecipes(event => {
+  event.add('kubejs:lemon_seed', 0.30)
 })
