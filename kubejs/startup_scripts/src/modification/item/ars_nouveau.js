53f8cefedb37ff32352e175a8d2c71416091d656
CelestialAbyss <30322176+CelestialAbyss@users.noreply.github.com>
2024-08-25 08:28:36 -0400

    Removed durability from Ars Elemental armor.

--- kubejs/startup_scripts/src/modification/item/ars_nouveau.js
+++ kubejs/startup_scripts/src/modification/item/ars_nouveau.js
@@ -14,4 +14,26 @@ ItemEvents.modification(event => {
     event.modify('ars_nouveau:' + element + '_robes', item => { item.maxDamage = -1 })
     event.modify('ars_nouveau:' + element + '_hood', item => { item.maxDamage = -1 })
   }
+  if (Platform.isLoaded('ars_elemental')) {
+    // fire
+    event.modify('ars_elemental:fire_hat', item => { item.maxDamage = -1 })
+    event.modify('ars_elemental:fire_robes', item => { item.maxDamage = -1 })
+    event.modify('ars_elemental:fire_leggings', item => { item.maxDamage = -1 })
+    event.modify('ars_elemental:fire_boots', item => { item.maxDamage = -1 })
+    // air
+    event.modify('ars_elemental:air_hat', item => { item.maxDamage = -1 })
+    event.modify('ars_elemental:air_robes', item => { item.maxDamage = -1 })
+    event.modify('ars_elemental:air_leggings', item => { item.maxDamage = -1 })
+    event.modify('ars_elemental:air_boots', item => { item.maxDamage = -1 })
+    // earth
+    event.modify('ars_elemental:earth_hat', item => { item.maxDamage = -1 })
+    event.modify('ars_elemental:earth_robes', item => { item.maxDamage = -1 })
+    event.modify('ars_elemental:earth_leggings', item => { item.maxDamage = -1 })
+    event.modify('ars_elemental:earth_boots', item => { item.maxDamage = -1 })
+    // aqua
+    event.modify('ars_elemental:aqua_hat', item => { item.maxDamage = -1 })
+    event.modify('ars_elemental:aqua_robes', item => { item.maxDamage = -1 })
+    event.modify('ars_elemental:aqua_leggings', item => { item.maxDamage = -1 })
+    event.modify('ars_elemental:aqua_boots', item => { item.maxDamage = -1 })
+  }
 })
