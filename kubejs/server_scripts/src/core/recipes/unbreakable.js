5b0494656fa08822d0aafbe411902d0e8a18a726
CelestialAbyss <30322176+CelestialAbyss@users.noreply.github.com>
2024-09-03 09:29:20 -0400

    Solution 2 for unbreakable tools is in the form of a recipe!

--- kubejs/server_scripts/src/core/recipes/unbreakable.js
+++ kubejs/server_scripts/src/core/recipes/unbreakable.js
@@ -5,36 +5,80 @@
  * @author ladylexxie <https://github.com/ladylexxie> Inventory changed
  */
 
+// requires: create
+// requires: farmersdelight
+// ignored: false
+
 let tools = ['sword', 'shovel', 'pickaxe', 'axe', 'hoe']
 let final_tools = ['sword', 'shovel', 'pickaxe', 'axe', 'hoe', 'lance', 'scythe', 'katana']
-let mod_tools = [
-  'farmersdelight:netherite_knife',
+let pickletweaks_tools = [
   'pickletweaks:netherite_paxel',
-  'pickletweaks:netheritesickle',
+  'pickletweaks:netherite_sickle',
   'pickletweaks:netherite_scythe'
 ]
 
+ServerEvents.recipes(event => {
+  for (let i = 0; i < tools.length; i++) {
+    let element = tools[i];
+    event.recipes.kubejs.shapeless(Item.of('minecraft:netherite_' + element).withNBT({ Unbreakable: 1 }), [
+      'minecraft:netherite_' + element,
+      'create:experience_block'
+    ]).id('kubejs:unbkreakable_netherite_' + element)
+    event.recipes.kubejs.shapeless(Item.of('kubejs:iridium_' + element).withNBT({ Unbreakable: 1 }), [
+      'minecraft:iridium_' + element,
+      'create:experience_block'
+    ]).id('kubejs:unbkreakable_iridium_' + element)
+  }
+  for (let i = 0; i < final_tools.length; i++) {
+    let element = final_tools[i];
+    event.recipes.kubejs.shapeless(Item.of('kubejs:final_' + element).withNBT({ Unbreakable: 1 }), [
+      'minecraft:final_' + element,
+      'create:experience_block'
+    ]).id('kubejs:unbkreakable_final_' + element)
+  }
+  event.recipes.kubejs.shapeless(Item.of('farmersdelight:netherite_knife').withNBT({ Unbreakable: 1 }), [
+    'farmersdelight:netherite_knife',
+    'create:experience_block'
+  ]).id('kubejs:farmersdelight/unbreakable_netherite_knife')
+  if (Platform.isLoaded('pickletweaks')) {
+    event.recipes.kubejs.shapeless(Item.of('pickletweaks:netherite_paxel').withNBT({ Unbreakable: 1 }), [
+      'pickletweaks:netherite_paxel',
+      'create:experience_block'
+    ]).id('kubejs:pickletweaks/unbreakable_netherite_paxel')
+    event.recipes.kubejs.shapeless(Item.of('pickletweaks:netherite_sickle').withNBT({ Unbreakable: 1 }), [
+      'pickletweaks:netherite_sickle',
+      'create:experience_block'
+    ]).id('kubejs:pickletweaks/unbreakable_netherite_sickle')
+    event.recipes.kubejs.shapeless(Item.of('pickletweaks:netherite_scythe').withNBT({ Unbreakable: 1 }), [
+      'pickletweaks:netherite_scythe',
+      'create:experience_block'
+    ]).id('kubejs:pickletweaks/unbreakable_netherite_scythe')
+  }
+})
+
+/*
 PlayerEvents.inventoryChanged(event => {
   const { item } = event
   for (let i = 0; i < tools.length; i++) {
     let element = tools[i];
-    if (item.hasNBT() && item.getId() == 'minecraft:netherite_' + element) {
-      item.nbt.merge({ Damage: 0, Unbreakable: 1 })
+    if (item.getId() == 'minecraft:netherite_' + element) {
+      item.nbt.merge({ Unbreakable: 1 })
     }
-    if (item.hasNBT() && item.getId() == 'kubejs:iridium_' + element) {
-      item.nbt.merge({ Damage: 0, Unbreakable: 1 })
+    if (item.getId() == 'kubejs:iridium_' + element) {
+      item.nbt.merge({ Unbreakable: 1 })
     }
   }
   for (let i = 0; i < final_tools.length; i++) {
     let element = final_tools[i];
-    if (item.hasNBT() && item.getId() == 'kubejs:final_' + element) {
-      item.nbt.merge({ Damage: 0, Unbreakable: 1 })
+    if (item.getId() == 'kubejs:final_' + element) {
+      item.nbt.merge({ Unbreakable: 1 })
     }
   }
   for (let i = 0; i < mod_tools.length; i++) {
     let element = mod_tools[i];
-    if (item.hasNBT() && item.getId() == element) {
-      item.nbt.merge({ Damage: 0, Unbreakable: 1 })
+    if (item.getId() == element) {
+      item.nbt.merge({ Unbreakable: 1 })
     }
   }
 })
+*/
